5b2f74e178c9d5c10fb91e440714bc54690d9b51
Daniel <Daniuu.Wikipedia@gmail.com>
2021-04-09 21:53:12 +0200

    Update Core.py
    Modifying file (the bot hasn't been approved yet for use on the Dutch Wikipedia)

--- Core.py
+++ Core.py
@@ -13,7 +13,7 @@ import time
 
 class Bot:
     'This class is designed to facilitate all interactions with Wikipedia (and to get the processing functions out of other calsses)'
-    max_edit = 5 #The maximum number of edits that a single bot can do per minute
+    max_edit = 1 #The maximum number of edits that a single bot can do per minute
     
     def __init__(self, api, m=None):
         'Constructs a bot, designed to interact with one Wikipedia'
