a613a9f3458ff4a3f361834bbe061e86ee53d103
Daniel <Daniuu.Wikipedia@gmail.com>
2021-04-11 00:20:34 +0200

    Update Admin patrol stable.py
    Adding some new features (to avoid edit conflicts etc.)

--- Admin patrol stable.py
+++ Admin patrol stable.py
@@ -42,22 +42,37 @@ class Page:
     def __str__(self):
         return self.name
     
-    def __call__(self):
-        return self.update()
+    def __call__(self, logonly=False):
+        return self.update(logonly)
     
     def get_page_content(self):
         "This function will get the last revision of the request page"
         d = {'action':'query',
              'prop':'revisions',
              'titles':self.name,
-             'rvprop':'content|ids',
+             'rvprop':'content|ids|timestamp',
              'rvlimit':1,
              'rvdir':'older'}
         jos = self.bot.get(d)['query']['pages']
         self.id = int(next(iter(jos.keys())))
+        self._timestamp = jos[str(self.id)]['revisions'][0]['timestamp'] #To check for an eventual edit conflict
         temp = next(iter(jos.values()))['revisions'][0]['*'].split('\n')
         self._content = [i.strip() for i in temp if i.strip()]
         return self._content
+
+    def check_request_on_line(self, line, pattern=r'((diff=\d{1,9}\&)?oldid=\d{1,9}|Permalink:\d{1,9}|\{\{diff\|\d{1,9})'):
+        "Checks whether the line passed as an argument contains any kind of requests"
+        raw = re.findall(pattern, line) #this will unleash the regex on the poor little line
+        z = [] #create a list to store all separate matches (and where we can leave out the empty matches if any)
+        for i in raw: #Go through all returned matches
+            if isinstance(i, (tuple, list)):
+                for j in i: #check all separate elements of the tuple or list that was found
+                    if j.strip(): #Check that j is not empty
+                        z.append(j.strip())
+            else: #We found a string or so, can just be added if not empty
+                if i:
+                    z.append(j)
+        return z #Returns the list with non-empty matches of the regex
     
     def separate(self, pend='Nieuwe verzoeken', hstart='Afgehandelde verzoeken'):
         "This function will separate the contents of the page into preamble, actual requests and handled ones"
@@ -75,88 +90,118 @@ class Page:
         self._done = self._content[hs:]
         return self._queue
     
-    def filter_queue(self, pattern=r'((diff=\d{1,9}\&)?oldid=\d{1,9}|Permalink:\d{1,9}|\{\{diff\|\d{1,9})'):
+    def filter_queue(self):
         "This function will convert the strings in the queue to a requests that can be handled"
         if not self._queue:
             #This means that the split was not yet done
             self.separate()
-        start = 1
+        #manually_flagged = False #this is a parameter that should check whether a given reqeust has been flagged manually
         try:
             for i, j in enumerate(self._queue[1:]):
                 #Skip the first one, this only contains a header for this section
-                zraw = re.findall(pattern, j)
-                z = []
-                for k in zraw:
-                    if isinstance(k, (tuple, list)):
-                        z += list(k)
-                    else:
-                        z.append(k)
-                z = [k for k in z if k]
+                z = self.check_request_on_line(j)
                 if z:
                     if i >= 1:
-                        self.requests.update({l:(start, i + 1)})
+                        if manually_flagged is True: #the request has been flagged in a manual manner
+                            self.requests['flagged'] = self.requests.get('flagged', []) + [(start, i + 1)]
+                        else:
+                            self.requests.update({l:(start, i + 1)})
                     l = tuple((Request(i) for i in z)) #Generate a tuple with the requests
                     start = i + 1
-            self.requests.update({l:(start, i + 2)})
+                    manually_flagged = False
+                elif any(((('{{' + k + '}}') in j) for k in Page.donetemp)):
+                    manually_flagged = True #Indicate that this request has been flagged manually
+            
+            #Process the request at the end of the queue
+            if manually_flagged is True:
+                self.requests['flagged'] = self.requests.get('flagged', []) + [(start, i + 2)]
+            else:
+                self.requests.update({l:(start, i + 2)})
         except UnboundLocalError:
-            return None
+            return None #Do nothing (this is due to the fact that the )
         return self.requests
     
     def check_queue_done(self):
+        "Check which requests in the queue are done (and flags them accordingly)"
         if not self.requests:
             self.filter_queue()
         for i in self.requests:
-            for j in i:
-                j.check_done(self.bot)
+            if not isinstance(i, str): #Ignore strings, these are only added for administrative purposes
+                for j in i:
+                    j.check_done(self.bot)
     
     def check_requests(self):
         'This function will check whether all requests are done, and can move the request to the next part'
         self.check_queue_done()
-        sto = []
+        sto = []# A list to store the indices that can be processed
+        #First, process the requests that were marked manually
+        sto += [(i, j, None) for i, j in self.requests.get('flagged', ())] #Generate a list of tuples with 'None' as third element
+        
+        #Now, process the requestst that can be flagged automatically
         for i in self.requests:
-            if all((bool(j) for j in i)):
-                sto.append(self.requests[i] + (i[0].check_person(self.bot),)) #Add the desired indices to the list that will be processed later
+            if not isinstance(i, str): #These ones should be ignored (we can do the deletion first)
+                if all((bool(j) for j in i)): #checks whether all requests have been handled
+                    sto.append(self.requests[i] + (i[0].check_person(self.bot),)) #Add the desired indices to the list that will be processed later
+        
+        #Begin processing the requests that are done or flagged
         sto.sort() #Do in place sorting to make things easier
         for i, j, u in sto: #Query the indices and add the request to the 'done' section
             self._done += self._queue[i:j]
-            pre = self._queue[j - 1].split()[0]
-            if "*" in pre:
-                prefix = '*'*(pre.count('*') + 1)
-            else:
-                prefix = ':'
-            self._done.append(prefix + '{{d}} - ' + f'Gevraagde versie(s) zijn verborgen door {u}. Dank voor de melding. ~~~~')
+            if u is not None: #u is None indicates that the request was manually flagged
+                pre = self._queue[j - 1].split()[0]
+                if "*" in pre:
+                    prefix = '*'*(pre.count('*') + 1)
+                else:
+                    prefix = ':'
+                self._done.append(prefix + '{{done}} - ' + f'Gevraagde versie(s) zijn verborgen door {u}. Dank voor de melding. ~~~~')
         for i, j, _ in sto[::-1]: #Scan in reverse order - this will make the deletion sequence more logical
             del self._queue[i:j]
         return len(sto) #Return the number of processed requests
     
     def check_removal(self, tz=('utc', 'cet', 'cest'), drm=1):
         'This function will check which lines could be removed (after drm days).'
+        def process(indices, matches): #Use a closure
+            #The date was found, now convert it to a format that Python acutally understands
+            date = matches[0]
+            for k in Page.nldate:
+                date = date.replace(k, Page.nldate[k])
+            d = dt.datetime.strptime(date, '%d %m %Y') #this is the object that can actually do the job for us
+            
+            #check whether we can get rid of this request
+            deltime = d + dt.timedelta(days=drm, hours=6) #Only remove the requests from 6 am onwards
+            if deltime < now:
+                l.append(indices)
+            
         if not self._done:
             self.separate()
         #Filter the requests (see also above)
-        l, start, now = [], 1, dt.datetime.now()  #Check what time it is
+        l, start, now, mark = [], 1, dt.datetime.now(), False  #Check what time it is (mark is used to verify that we found a nice match)
         pat = r'(\d{1,2} ' + f'({"|".join(Page.nldate.keys())}) ' + r'\d{4})'
         for i, j in enumerate(self._done):
-            if i > 0:
+            if i > 1: #Ignore the first line (and second line, to make things easier)
+                if self.check_request_on_line(j):
+                    #We ended searching our current request, add it to the list if it can be deleted
+                    if mark is False:
+                        temp = self.check_request_on_line(self._done[i - 1].lower(), pat)
+                        if temp:
+                            matches = temp
+                    process((start, i), matches) #rely on the closure to process the request
+                    start = i #Set for the processing of the next request
+                    mark = False #Reset this
+
                 if any(('{{%s}}'%k in j for k in Page.donetemp)):
                     #Process the request
-                    matches = re.findall(pat, j.lower())
-                    #The date was found, now convert it to a format that Python acutally understands
-                    date = matches[0][0]
-                    for k in Page.nldate:
-                        date = date.replace(k, Page.nldate[k])
-                    d = dt.datetime.strptime(date, '%d %m %Y') #this is the object that can actually do the job for us
-                    
-                    #check whether we can get rid of this request
-                    deltime = d + dt.timedelta(days=drm, hours=6) #Only remove the requests from 6 am onwards
-                    if deltime < now:
-                        l.append((start, i + 1))
-                    start = i + 1 #Set for the processing of the next request
+                    mark = True
+                    matches = self.check_request_on_line(j.lower(), pat)
+        
+        #Process the final request
+        process((start, i + 1), matches)
+        print(l)
         for i, j in l[::-1]:
             del self._done[i:j]
         return len(l) #Return the amount of requests that were deleted
                 
-    def update(self):
+    def update(self, logonly=False):
         "This function will update the content of the page"
         y = self.check_removal() #How many requests are deleted
         z = self.check_requests()
@@ -173,9 +218,17 @@ class Page:
                     'summary':summary,
                     'bot':True,
                     'minor':True,
-                    'nocreate':True}
-        #self.bot.post(edit_dic) #Make the post request - comment this rule for log only
-        print(y, z)
+                    'nocreate':True,
+                    'basetimestamp':self._timestamp}
+        if logonly is False:
+            result = self.bot.post(edit_dic) #Make the post request and store the output to check for eventual edit conflicts
+            if 'error' in result: #An error occured
+                if result['error']['code'] == 'editconflict':
+                    print('An edit conflict occured during the processing. I will wait for ten seconds')
+                    time.sleep(10)
+                    print('Redoing the things.')
+                    return self() #Rerun the script, we found a nice little new request
+        print(f'Removed {y}, processed {z}') #Just some code for maintenance purposes
                 
 class Request:
     "This object class will implement the main functionalities for a certain request"
@@ -239,4 +292,4 @@ class Request:
         return f"De versie(s) is/zijn verborgen door {self._user if self._user is not None else 'een moderator'}"
 
 t = Page("Wikipedia:Verzoekpagina voor moderatoren/Versies verbergen", "https://nl.wikipedia.org/wiki/Wikipedia:Verzoekpagina_voor_moderatoren/Versies_verbergen")
-t()
+t(True) #Script in log-only 
