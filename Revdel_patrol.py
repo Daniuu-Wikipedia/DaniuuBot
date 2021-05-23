b1f93a1b9e2837a123de93b4b4c214f23a0dcf79
Daniuu <Daniuu.wikipedia@gmail.com>
2021-05-23 02:35:19 +0200

    Update Revdel_patrol.py

--- Revdel_patrol.py
+++ Revdel_patrol.py
@@ -52,8 +52,10 @@ class Page:
              'rvprop':'content|ids|timestamp',
              'rvlimit':1,
              'rvdir':'older'}
-        jos = self.bot.get(d)['query']['pages']
-	self.id = int(next(iter(jos.keys())))
+        jos = self.bot.get(d)
+        print(jos)
+        jos = jos['query']['pages']
+        self.id = int(next(iter(jos.keys())))
         self._timestamp = jos[str(self.id)]['revisions'][0]['timestamp'] #To check for an eventual edit conflict
         temp = next(iter(jos.values()))['revisions'][0]['*'].split('\n')
         self._content = [i.strip() for i in temp if i.strip()]
@@ -163,7 +165,7 @@ class Page:
                     prefix = '*'*(pre.count('*') + 1)
                 else:
                     prefix = ':'
-                self._done.append(prefix + '{{done}} - ' + '%s Dank voor de melding. ~~~~'%u)
+                self._done.append(prefix + '{{d}} - ' + '%s Dank voor de melding. ~~~~'%u)
         for i, j, _ in sto[::-1]: #Scan in reverse order - this will make the deletion sequence more logical
             del self._queue[i:j]
         return len(sto) #Return the number of processed requests
