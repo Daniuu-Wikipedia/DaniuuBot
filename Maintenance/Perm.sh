75234debfca51dbd869d32f50a0e348f287aa5fd
Daniuu <Daniuu.Wikipedia@gmail.com>
2022-09-17 20:16:44 +0200

    Bugfix

--- Maintenance/Perm.sh
+++ Maintenance/Perm.sh
@@ -7,4 +7,4 @@ find ~/DaniuuBot -name "*.txt" -exec chmod 600 {} \;
 
 #Prohibit the reading of shell files in the home directory by third parties
 cd ~
-chmod 700 *.sh
+find ~ -maxdepth 0 -name "*.sh" -exec chmod 700 {} \;
