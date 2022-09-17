3a2d558ac8a23c7f898727f97a4ae01e8aa3f6db
Daniuu <Daniuu.Wikipedia@gmail.com>
2022-09-17 22:16:50 +0200

    Create maintenance scripts

---
+++ Toolforge_venv/Delete_venv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Launch from the main directory
+cd ~
+
+#Delete the venv
+rm -r botenv/ #Update the name of the venv when needed
---
+++ Toolforge_venv/Reset_venv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Delete the venv
+bash ~/DaniuuBot/Toolforge_venv/Delete_venv.sh
+
+#Recreate the venv
+bash ~/DaniuuBot/Toolforge_venv/Create_venv.sh
