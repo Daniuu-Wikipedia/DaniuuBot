ec3893b49c1f4ce05a4306da95d57ecfeaecce3a
Daniuu <Daniuu.Wikipedia@gmail.com>
2022-09-17 21:39:07 +0200

    Create Create_venv.sh

---
+++ Toolforge_venv/Create_venv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# use bash strict mode
+set -euo pipefail
+
+# create the venv
+python3 -m venv botenv
+
+# activate it
+source botenv/bin/activate
+
+# upgrade pip inside the venv and add support for the wheel package format
+pip install -U pip wheel
+
+# install some concrete packages
+pip install sys
+pip install os
+pip install requests
+pip install pyyaml
+pip install datetime
+pip install re
+pip install requests_oauthlib
+pip install time
+pip install math
