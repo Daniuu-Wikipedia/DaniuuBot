e7a2c3a2a592289bc6bfca5170205da053c29fea
Daniuu <Daniuu.Wikipedia@gmail.com>
2022-09-18 03:09:32 +0200

    Initial commit

---
+++ CVN_Watchlist/Timezones.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 03:08:34 2022
+
+@author: Daniuu
+"""
---
+++ CVN_Watchlist/Timezones_Data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 03:08:48 2022
+
+@author: Daniuu
+
+This file is solely inteded for the storage of data in a Python-readable format.
+"""
