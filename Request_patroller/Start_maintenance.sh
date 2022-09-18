cc5cc802758441712c13e70a5e1d7c5d3c2d92a9
Daniuu <Daniuu.Wikipedia@gmail.com>
2022-09-18 02:11:13 +0200

    Fix

--- Request_patroller/Start_maintenance.sh
+++ Request_patroller/Start_maintenance.sh
@@ -14,6 +14,6 @@ MAGENTA='\033[35m'
 #Submits the job to stop
 echo -e "${RED}Stopping${NOCOLOR} the bot. I will submit the stopping Python script."
 
-toolforge-jobs run restart-bot --command "./botenv/bin/python ./DaniuuBot/Request_patroller/Py_Start_maintenance" --image tf-python39 
+toolforge-jobs run restart-bot --command "./botenv/bin/python ./DaniuuBot/Request_patroller/Py_Start_maintenance.py" --image tf-python39 
 
-echo -e "$Job {GREEN}sumbitted."
+echo -e "Job ${GREEN}sumbitted."
