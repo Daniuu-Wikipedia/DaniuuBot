5e05b192455c4f220e944586395cab890c425473
Daniuu <Daniuu.Wikipedia@gmail.com>
2022-09-19 19:02:44 +0200

    Finish Part & date management
    Class 'Part' completed and tested - the functions for date management were inserted.

--- CVN_Watchlist/CVN_watchlist_patroller.py
+++ CVN_Watchlist/CVN_watchlist_patroller.py
@@ -13,19 +13,50 @@ The functionality for the time zones is listed in two other files.
 import Core_CVN as core
 import datetime as dt
 import re 
+from Timezones import offset
+
+def to_UTC(date):
+    "Converts a given date in Amsterdam time to UTC"
+    if not isinstance(date, dt.datetime):
+        raise TypeError('Only valid dateTIME-instances can be processed by the to_UTC function!')
+    return date + offset(date) #offset from the separate script
 
 def format_date(date):
-    "Converts a datetime.datetime instance into an ISO-8601 format (supported by the Wikimedia API)"
+    """Converts a datetime.datetime instance into an ISO-8601 format (supported by the Wikimedia API).
+    The function automatically converts a given LOCAL time into a UTC-format. 
+    This function allows the code to use local times everywhere.
+    """
     if not isinstance(date, dt.datetime):
         raise TypeError('Please pass a valid datetime.datetime object')
-    return date.isoformat() #L = we are using local time of the wiki
+    return f'{to_UTC(date).isoformat()}Z' #L = we are using local time of the wiki - Z = UTC time
 
+def wikidate(d, showday):
+    """
+    This function converts a give day into a format that can be used on the wiki (human-readable)
+
+    Parameters
+    ----------
+    d : dt.datetime
+        The date that is printed to the wiki.
+    showday : boolean
+        Determines whether or not the date is printed.
+    """
+    timestring = f'{d.hour:02d}:{d.minute:02d}'
+    if not isinstance(showday, bool):
+        raise TypeError('Showday must be a boolean, not %s'%showday)
+    if showday is True:
+        return f'{d.day:02d}-{d.month:02d}-{d.year:02d} {timestring}'
+    return timestring
+            
 class Part:
     """
     The class processes one _day part_
     Arguments: start- and end-time. A dedicated script will be developed to handle time zone changes.
     Times should be passed as datetime.datetime in utc.
     """
+    api = core.NlBot() #Use one bot for all queries
+    template = 'Gebruiker:Krinkle/cvlijst2/rclink' #The template that is used to link to the revisions to be patrolled
+    
     def __init__(self, start, end):
         assert start <= end, "The order of the arguments of a datepart got inverted!"
         self.start, self.end = start, end
@@ -35,19 +66,37 @@ class Part:
     def get_unpatrolled_edits(self):
         pay = {'action':'query',
                'list':'recentchanges',
-               'rcdir':'newer',
+               'rcdir':'newer', #This parameter must be conserved if you don't want to break the bot
                'rcstart':format_date(self.start),
                'rcend':format_date(self.end),
                'rcprop':'title',
-               'rclimit':3, #At this point, we only want to know whether or not there are unpatrolled edits.
+               'rclimit':10, #At this point, we only want to know whether or not there are unpatrolled edits.
                'rcshow':'anon|unpatrolled'}
+        self.nredits = len(Part.api.get(pay)['query']['recentchanges'])
     
     def __eq__(self, other):
         return self.start == other.start and self.end == other.end
     
     def __lt__(self, other):
         return self.start < other.start
-
+    
+    def __len__(self):
+        return self.nredits
+    
+    def __bool__(self):
+        return self.nredits == 0 #Check if there are still edits that should be patrolled
+    
+    def __str__(self):
+        "This function converts a slot into a format that can be interpreted by humans"
+        #General prefix (always mentioned: the times)
+        timestring = f'{wikidate(self.start, False)} - {wikidate(self.end, False)}:'
+        if self: #Self bool(self) is True, all edits were patrolled
+            return "%s {{d|'''Gecontroleerd'''}} - ~~~~"%timestring
+        #There are still edits that need patrolling
+        tempar = "{{%s|%s|%s}}"%(Part.template, wikidate(self.start, True), wikidate(self.end, True))
+        return f'{timestring} {tempar}'
+        
+        
 #Process one single day
 class Day:
     """
@@ -84,9 +133,7 @@ class Day:
     
 #Some testcode
 if __name__ == '__main__':
-    print('Running check')
     bot = core.NlBot()
-    print(format_date(dt.datetime(2022, 9, 6, 0, 0)))
     pay = {'action':'query',
            'list':'recentchanges',
            'rcdir':'newer',
@@ -95,7 +142,12 @@ if __name__ == '__main__':
            'rcprop':'title|user|timestamp',
            'rclimit':50,
            'rcshow':'anon|unpatrolled'}
-    repl = (bot.get(pay))
-    print(repl)
-    print(len(repl['query']['recentchanges']))
-    
+
+#Other testcode
+k = dt.datetime(2022, 11, 1, 9, 0)
+
+pt = Part(dt.datetime(2022, 9, 6, 0, 0, 0), dt.datetime(2022, 9, 6, 6, 0, 0))
+print(pt)
+
+real = r"00:00 - 06:00: {{Gebruiker:Krinkle/cvlijst2/rclink|06-09-2022 00:00|06-09-2022 06:00}}"
+print(str(pt) == real)
