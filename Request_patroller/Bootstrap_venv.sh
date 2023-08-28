5da4f08084c96e92af92f80aa3e5973bb69ee9e6
Daniuu <Daniuu.Wikipedia@gmail.com>
2023-08-29 01:32:54 +0200

    Create additional shell script to set up the bot's virtual environment

---
+++ Request_patroller/Bootstrap_venv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shell file to create the virtual environment using the desired Python interpreter
+cd ~
+
+# Chmod shell script, so it can be called directly by toolforge-jobs
+chmod ug+x Create_venv.sh
+
+# This command effectively starts the process of creating the environment
+# Specify the --wait parameter! Other wise, the file will be chmodded back prematurely
+toolforge-jobs run create-venv --command "cd $PWD && ./Create_venv.sh" --image python3.9 --wait
+
+# Chmod it back, to stop it from being executed directly
+chmod 740 Create_venv.sh
