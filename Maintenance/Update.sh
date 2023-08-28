64206b843e8139980e0b8a51175505e2d3e2e9d4
Daniuu <Daniuu.Wikipedia@gmail.com>
2023-08-29 01:36:15 +0200

    Update tools for maintenance

--- Maintenance/Update.sh
+++ Maintenance/Update.sh
@@ -7,9 +7,15 @@ rm */*/*.pyc
 
 git pull git@github.com:Daniuu-Wikipedia/DaniuuBot.git
 
+
+# Move scripts in the "Maintanance section" to a home directory - easier access
 cd ~/DaniuuBot/Maintenance
 
 cp *.sh ~/Handy_scripts
+
+# Also move the scripts relating to the Virtual environment
+cd ~/DaniuuBot/Toolforge_venv
+cp *.sh ~/Tools_venv
 
 # Safety measure: make sure that all .txt-files are properly chmodded (and invisble to other users)
 cd ~/Handy_scripts
--- Toolforge_venv/Reset_venv.sh
+++ Toolforge_venv/Reset_venv.sh
@@ -4,4 +4,4 @@
 bash ~/DaniuuBot/Toolforge_venv/Delete_venv.sh
 
 #Recreate the venv
-toolforge-jobs run venv-create --command "cd $PWD && ./DaniuuBot/Toolforge_venv/Create_venv.sh" --image tf-bullseye-std --wait
+bash Bootstrap_venv.sh
