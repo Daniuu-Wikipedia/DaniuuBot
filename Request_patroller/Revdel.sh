a3c72eb6b7ebf7b4a696569e917d92fb692f4fae
Daniuu <Daniuu.Wikipedia@gmail.com>
2023-10-23 20:26:54 +0200

    Image issue

--- Request_patroller/Revdel.sh
+++ Request_patroller/Revdel.sh
@@ -2,8 +2,5 @@
 
 #For immediate execution of the revdel script
 
-python -V
-python3 -V
-
-toolforge-jobs run revdel2 --command "./botenv/bin/python3.11 ./DaniuuBot/Request_patroller/Revdel_patrol.py" --image tf-python3.11 --wait
+toolforge-jobs run revdel2 --command "./botenv/bin/python3.11 ./DaniuuBot/Request_patroller/Revdel_patrol.py" --image python3.11 --wait
 
