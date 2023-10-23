dddd402e66e5196f2af68e09e2cb1d175c808172
Daniuu <Daniuu.Wikipedia@gmail.com>
2023-10-23 21:07:38 +0200

    Migrate to Python 3.11

--- Reset_Toolforge_jobs.sh
+++ Reset_Toolforge_jobs.sh
@@ -18,17 +18,20 @@ echo -e "${YELLOW}I have deleted all existing jobs."
 echo -e "${NOCOLOR}I will now resubmit the required jobs"
 
 #Job to patrol https://nl.wikipedia.org/wiki/Wikipedia:Verzoekpagina_voor_moderatoren/Versies_verbergen
-toolforge-jobs run revdel --command "./botenv/bin/python ./DaniuuBot/Request_patroller/Revdel_patrol.py" --image tf-python39 --schedule "*/15 * * * *"
+toolforge-jobs run revdel --command "./botenv/bin/python ./DaniuuBot/Request_patroller/Revdel_patrol.py" --image python3.11 --schedule "*/15 * * * *"
 
 echo -e "Job to patrol WP:VV ${GREEN}successfully${NOCOLOR} submitted to the ${CYAN}Kubernetes engine${NOCOLOR}."
 
 #Job to patrol https://nl.wikipedia.org/wiki/Wikipedia:Verzoekpagina_voor_moderatoren/IPBlok
-toolforge-jobs run ipblok --command "./botenv/bin/python ./DaniuuBot/Request_patroller/IPBLOK_patrol.py" --image tf-python39 --schedule "02,12,22,32,42,52 * * * *"
+toolforge-jobs run ipblok --command "./botenv/bin/python ./DaniuuBot/Request_patroller/IPBLOK_patrol.py" --image python3.11 --schedule "02,12,22,32,42,52 * * * *"
 
 echo -e "Job to patrol WP:IPBLOK ${GREEN}successfully${NOCOLOR} submitted to the ${CYAN}Kubernetes engine${NOCOLOR}."
 
 #Job to synchonize my nlwiki and vlswiki user pages
-toolforge-jobs run up-sync --command "./botenv/bin/python ./DaniuuBot/Userpage_synchroniser/Copy_userpage.py" --image tf-python39 --schedule "20 4 * * *"
+# This job was stopped on 2023-10-23
+# toolforge-jobs run up-sync --command "./botenv/bin/python ./DaniuuBot/Userpage_synchroniser/Copy_userpage.py" --image python3.11 --schedule "20 4 * * *"
 
-echo -e "Job to sync your vlswiki and nlwiki user pages ${GREEN}successfully${NOCOLOR} submitted to the ${CYAN}Kubernetes engine${NOCOLOR}."
+# echo -e "Job to sync your vlswiki and nlwiki user pages ${GREEN}successfully${NOCOLOR} submitted to the ${CYAN}Kubernetes engine${NOCOLOR}."
 
+# Just write a message that we're done submitting jobs to the grid
+echo -e "I have ${GREEN}SUCCESSFULLY${NOCOLOR} submitted the jobs I had to submit!"
