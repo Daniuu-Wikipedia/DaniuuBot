4211c8769972c9609a6b33c403b2f50eb53e5aca
Daniuu-Wikipedia <Daniuu.Wikipedia@gmail.com>
2024-07-12 18:52:18 +0200

    Typo fix

--- Request_patroller/Patrol_request.sh
+++ Request_patroller/Patrol_request.sh
@@ -2,4 +2,4 @@
 
 #For immediate execution of the request patroller scripts
 
-toolforge-jobs run patroller_all --command "./botenv/bin/python3.11 ./DaniuuBot/Request_patroller/Run_all.py" --image python3.11 --wait
+toolforge-jobs run patroller-all --command "./botenv/bin/python3.11 ./DaniuuBot/Request_patroller/Run_all.py" --image python3.11 --wait
