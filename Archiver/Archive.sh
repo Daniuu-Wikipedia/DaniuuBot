96a9ddf4e483c5d0dea43abf8686783416cce13c
Daniuu-Wikipedia <Daniuu.Wikipedia@gmail.com>
2024-07-12 18:14:50 +0200

    Add bash script for Toolforge

---
+++ Archiver/Archive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#For immediate execution of the revdel script
+
+toolforge-jobs run ipblok2 --command "./botenv/bin/python3.11 ./DaniuuBot/Archiver/Run_all.py" --image python3.11 --wait
