26c44f889d68c53b2d6e84abcfb29da07d109c3f
Daniuu-Wikipedia <Daniuu.Wikipedia@gmail.com>
2024-11-02 14:28:33 +0100

    Prepare script for use with REGBLOK checker

---
+++ Request_patroller/REGBLOK_approved_reasons.py
@@ -0,0 +1,15 @@
+"""
+This module contains a list of block reasons that allow handling by the bot.
+
+Two lists are implemented:
+    1) `regblok_contains`: contains some key words. If these key words are present in the block reason, the bot will handle the request
+    2) `regblok-matchesµ: the bot will only handle the request if this block reason is used exactly
+"""
+
+regblok_contains = ['vandaal',
+                    'vandalisme',
+                    'spam',
+                    'spambot',
+                    'ongewenste gebruikersnaam']
+
+regblok_matches = ['On request, to test a script']  # Just for development purposes
