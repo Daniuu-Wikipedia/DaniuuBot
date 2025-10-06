3c56b9c5e68fae8cc059325e82a7108ee61cf5c4
Daniuu-Wikipedia <Daniuu.Wikipedia@gmail.com>
2025-10-06 23:32:04 +0200

    Update package list

--- Toolforge_venv/Create_venv.sh
+++ Toolforge_venv/Create_venv.sh
@@ -19,9 +19,12 @@ pip install -U pip wheel
 # install some concrete packages
 pip install requests
 pip install pyyaml
+pip install mwoauth
+pip install Flask
 pip install requests_oauthlib
 pip install toolforge
 pip install pytz  # To deal with timezones
 pip install PyMySQL  # Sync with nlwikibots requirements
 pip install pywikibot  # Not used by DaniuuBot, but required for other nlwikibots repositories
 pip install mwparserfromhell  # Also a pretty firm requirement :)
+pip install mwcomposerfromhell  # One more package - to transform into
